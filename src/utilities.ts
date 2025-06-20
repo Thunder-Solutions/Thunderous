c05a9f9cb8c9fd4a4b586aaf83b950713e8531c6
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2025-06-20 19:50:59 -0400

    remove unused utility

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,14 +1,5 @@
 export const NOOP = () => void 0;
 
-export const assumeObj = (obj: unknown): Record<PropertyKey, unknown> => {
-	if (typeof obj !== 'object' || obj === null) {
-		const error = new Error('Expected an object.');
-		console.error(error);
-		throw error;
-	}
-	return obj as Record<PropertyKey, unknown>;
-};
-
 export const queryComment = (node: Node, comment: string) => {
 	for (const child of node.childNodes) {
 		if (child.nodeType === Node.COMMENT_NODE && child.nodeValue === comment) {
