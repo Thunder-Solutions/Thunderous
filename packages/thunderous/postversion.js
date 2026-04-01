3c6c842eeaba8230889982c2424ed8874039ee5b
Jon DeWitt <jonathan.c.dewitt@gmail.com>
2026-04-01 00:49:15 -0400

    lift common dependencies to top leel

--- packages/thunderous/postversion.js
+++ packages/thunderous/postversion.js
@@ -3,6 +3,6 @@ import { spawnSync } from 'child_process';
 const [oldMajor, oldMinor] = process.env.npm_old_version.split('.');
 const [newMajor, newMinor] = process.env.npm_new_version.split('.');
 const type = oldMajor !== newMajor ? 'major' : oldMinor !== newMinor ? 'minor' : 'patch';
-spawnSync('npm', ['version', type], { cwd: `${process.cwd()}/www` });
-spawnSync('npm', ['version', type], { cwd: `${process.cwd()}/demo` });
+spawnSync('npm', ['version', type], { cwd: `${import.meta.dirname}/../../www` });
+spawnSync('npm', ['version', type], { cwd: `${import.meta.dirname}/demo` });
 spawnSync('git', ['add', '-A']);
